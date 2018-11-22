7dc2980ace64f8e1e87b3f377219516113f62ea7
Mariusz Kowalski <mariuszek@polish4you.waw.pl>
2018-11-22 21:02:41 +0100

    Rename mouse event handler function name

--- newborn.py
+++ newborn.py
@@ -1,14 +1,22 @@
 from tkinter import*
 from random import randint
 import time
+import math
 
 gui=Tk()
 
-def znajdywanie(event):
+def handle_mouse_event(event):
+    move_head(event)
     return(event.x, event.y)
 
 c=Canvas(gui, width=800, height=800, bg='white')
-c.bind('<Motion>', znajdywanie)
+c.bind('<Motion>', handle_mouse_event)
+
+bodypart= c.create_rectangle(10,10,20,20, fill='black')
+
+def move_head(event):
+    c.move(bodypart, event.x, event.y)
+
 c.pack()
 
 gui.mainloop()
