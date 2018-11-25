baea8c194410a37dd95fe93d9fd1f57671bd0e32
Mariusz Kowalski <mariuszek@polish4you.waw.pl>
2018-11-25 13:25:04 +0100

    Add board and apple controller - including eat detection

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 from tkinter import*
-from random import randint
+
+import random
 import time
 import math
 
@@ -32,7 +33,7 @@ class VelocityController:
     def set_head_pos(self, head_pos):
         self.xh = head_pos.x
         self.yh = head_pos.y
-        
+
     def recalculate_vx(self, xm, ym):
         return((xm - self.xh)/  3)
 
@@ -41,35 +42,71 @@ class VelocityController:
 
 class SnakeController:
 
-    def __init__(self, velocityController, delay):
-        self.delay = delay
+    def __init__(self, velocityController):
         self.velocityController = velocityController
 
     def start(self):
-        self.create_head()
-        
+        self.create_head()        
         self.velocityController.start()
-        self.next_frame()
-
-    def next_frame(self):
-        gui.after(self.delay, self.update_frame)
 
     def get_head_pos(self):
         coords = frame.coords(self.snake_head)
         return(Position(coords[0], coords[1]))
 
     def update_frame(self):        
-        frame.move(self.snake_head, self.velocityController.vx, self.velocityController.vy)    
-        self.velocityController.set_head_pos(self.get_head_pos())
+        frame.move(self.snake_head, self.velocityController.vx, self.velocityController.vy)
+        self.velocityController.set_head_pos(self.get_head_pos())        
+
+    def create_head(self):
+        self.snake_head = frame.create_rectangle(10, 10, 30, 30, fill = 'white')
+
+class AppleController:
+
+    def __init__(self):
+        self.x = random.randint(0, 780)
+        self.x2 = self.x + 20
+        self.y = random.randint(0, 780)
+        self.y2 = self.y + 20
+
+    def draw_apple(self):
+        frame.create_rectangle(self.x, self.y, self.x2, self.y2, fill = 'white')
+
+    def snake_eat_apple(self, snakeController):
+        snake_head_pos = snakeController.get_head_pos()
+        if self.x <= snake_head_pos.x <= self.x2 and self.y <= snake_head_pos.y <= self.y2:
+            print("zjadł")
+
+    def start(self):
+        self.draw_apple()        
+
+    def update_frame(self, snakeController):
+        self.snake_eat_apple(snakeController)
+
+class BoardController:
+    def __init__(self, snakeController, appleController, delay):
+        self.snakeController = snakeController
+        self.appleController = appleController
+        self.delay = delay
+
+    def start(self):
+        self.snakeController.start()
+        self.appleController.start();
         
         self.next_frame()
 
-    def create_head(self):
-        self.snake_head = frame.create_rectangle(10, 10, 30, 30, fill = 'white')        
+    def next_frame(self):
+        gui.after(self.delay, self.update_frame)
 
+
+    def update_frame(self):
+        self.snakeController.update_frame()
+        self.appleController.update_frame(self.snakeController)
+        
+        self.next_frame()        
+            
 frame = Canvas(gui, width = 800, height = 800, bg = 'black')
 frame.pack()
 
-SnakeController(VelocityController(20, 30), 150).start()
+BoardController(SnakeController(VelocityController(20, 30)), AppleController(), 150).start() 
 
 gui.mainloop()
